c840467264a7c9b571297361e10d60013270cc64
Igor <dumanschi.igor@gmail.com>
2024-10-15 12:43:20 +0300

    JS_7

---
+++ main.js
@@ -0,0 +1,23 @@
+// const str = 'string'
+
+// console.log(str.split(''))
+
+// console.log(str.split('').join(''))
+
+// const list = [3, 4, 5, 6]
+
+// const listFinal = list.reduce((acc, curr) => {
+// 	acc.push(curr - 1);
+// 	return acc
+// }, [])
+
+// console.log(listFinal)
+
+// const users = ['Alex', 'Victor', 'Artur']
+
+// const firstLetter = users.reduce((acc, curr) => {
+// 	acc.push(curr[0] + ' - prima litera din cuvint')
+// 	return acc
+// }, [])
+
+// console.log(firstLetter)
